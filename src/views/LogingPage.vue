654873851ddcf0f11e4581b6344a384c1b595118
MSI\gn01792218 <gn01792218@hotmail.com>
2022-10-09 22:08:06 +0800

    增加箭頭動畫

--- src/views/LogingPage.vue
+++ src/views/LogingPage.vue
@@ -12,7 +12,7 @@
       </div> -->
       <!-- loging內容 -->
       <div id="logBox" class="relative w-[90%] mx-auto flex flex-col justify-center">
-        <img v-show="onScroll" class="fixed bottom-[10%] right-[10%] hover:opacity-70 hover:scale-95" :src="getAssetsFileURL('images/toBottom.png')" alt="向下" @click="toBottom">
+        <img v-show="onScroll" class="fixed bottom-[10%] right-[10%] hover:opacity-70 hover:scale-95 animate-bounce" :src="getAssetsFileURL('images/toBottom.png')" alt="向下" @click="toBottom">
         <WsMsg class="mb-1" v-for="log in logArr" :key="log.message"
           :log-msg="log"
         />
